cc02b0a3c4d1c706bb8c6d8b7915b0d8ca5fc50b
Zachary Harris <the1zachharris@gmail.com>
2024-02-16 15:05:06 -0600

    /restaurants route

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
-
+app.get("/restaurants", async (req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+});
 
 
 module.exports = app;
