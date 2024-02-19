73fcd71a6ee1dd460c6c579b5763252018883e27
Zachary Harris <the1zachharris@gmail.com>
2024-02-19 10:44:47 -0600

    restraunts/id route

--- src/app.js
+++ src/app.js
@@ -5,9 +5,17 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+
+
 app.get("/restaurants", async (req, res) => {
     const restaurants = await Restaurant.findAll();
     res.json(restaurants);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+    const id = req.params.id;
+    const myRestaurant = await Restaurant.findByPk(id);
+    res.json(myRestaurant);
 });
 
 
